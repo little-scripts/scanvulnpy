6a40207646101f9238cd16f8c9fef2940a5b442a
little-scripts <jgdevrennes@gmail.com>
2024-03-15 22:23:37 +0100

    Style

--- scanvulnpy/__main__.py
+++ scanvulnpy/__main__.py
@@ -32,7 +32,6 @@ from .__version__ import (
     __version__,
 )
 
-
 def main():
     """
     Scanner vulnerability PyPI Packages.
@@ -40,22 +39,20 @@ def main():
     if not Utils.check_platform:
         print("\nThe script doesn't support your platform for the moment !\nFeel free to report issues: https://github.com/little-scripts/scanvulnpy/issues")
         sys.exit(0)
+    else:
+        print_banner(__author__, __version__)
 
-    # ------------ setup (Instantiate Object) ------------
+    # Setup (Instantiate Object)
     options = cmd_options()
     utils = Utils()
     scanvuln = VulnerabilityScanner()
     logger = Logger()
 
-    # ------------ start scan ------------
-    print_banner(__author__, __version__)
-
     # Get packages
     logger.info("Get PyPI packages, this may take few seconds...")
     packages, nb_packages = utils.get_requirements(options.requirements, options.freeze)
-
-    # Log start of the Scan
-    logger.info(f"Scan vulnerability on {nb_packages} PyPI packages")
+    if not packages:
+        sys.exit(1)
 
     # Initialize counters and lists to store results
     count_non_vulnerable = int(0)
@@ -63,6 +60,8 @@ def main():
     list_packages_non_vulnerable = []
     list_packages_vulnerable = []
 
+    # Log start of the Scan
+    logger.info(f"Scan vulnerability on {nb_packages} PyPI packages")
     # Iterate over packages and Scan each one
     for package in packages:
         # Set payload and header for request the API endpoint
